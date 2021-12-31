2740802476a2ee4f32b45c98864e5726913f64a8
AxelTahmid <axel.tahmid@gmail.com>
2022-01-01 01:46:47 +0600

    added user ->up migrations

--- database/migrations/2021_12_31_193712_create_users_table.php
+++ database/migrations/2021_12_31_193712_create_users_table.php
@@ -15,6 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique()->notNullable();
+            $table->string('password');
+            $table->tinyInteger('is_super_admin')->default(0); //for admin panel
             $table->timestamps();
         });
     }
