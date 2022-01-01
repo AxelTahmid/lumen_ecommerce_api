c77fb9d43578cf837ea494bfb773f3bf702e58b2
AxelTahmid <axel.tahmid@gmail.com>
2022-01-01 16:20:37 +0600

    added brand and products migration

--- database/migrations/2021_12_31_193740_create_brands_table.php
+++ database/migrations/2021_12_31_193740_create_brands_table.php
@@ -15,6 +15,7 @@ class CreateBrandsTable extends Migration
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
             $table->timestamps();
         });
     }
--- database/migrations/2021_12_31_193745_create_products_table.php
+++ database/migrations/2021_12_31_193745_create_products_table.php
@@ -15,7 +15,23 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->decimal('price');
+            $table->integer('amount');
+            $table->integer('discount')->nullable();
+            $table->date('discount_start_date')->nullable();
+            $table->date('discount_end_date')->nullable();
+            $table->bigInteger('created_by')->nullable()->unsigned();
+            $table->bigInteger('category_id')->nullable()->unsigned();
+            $table->bigInteger('brand_id')->nullable()->unsigned();
+            $table->string('product_code')->nullable();
+            $table->tinyInteger('featured')->default(0);
             $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('set null');
         });
     }
 
