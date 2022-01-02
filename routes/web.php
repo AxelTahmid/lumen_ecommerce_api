8cb6cc45bc9733a832e4702e0ffd2c4893ed091a
AxelTahmid <axel.tahmid@gmail.com>
2022-01-02 19:23:50 +0600

    Auth Starter Branch

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ $router->get('/', function () use ($router) {
 
 $router->group(['prefix' => 'api'], function () use ($router) {
     $router->post('/login', 'Auth\\LoginController@login');
+    $router->post('/register', 'Auth\\RegisterController@register');
 
     $router->group(['middleware' => 'auth:api'], function () use ($router) {
         $router->get('/me', 'Auth\\LoginController@userDetails');
