0e7d4751f74a6b34b0107bfee75cc11ba3be14c2
AxelTahmid <axel.tahmid@gmail.com>
2022-01-02 19:00:03 +0600

    added register endpoint

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace App\Http\Controllers\Auth;
+
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\JWTAuth;
+
+class RegisterController extends Controller
+{
+    protected $jwt;
+
+    public function __construct(JWTAuth $jwt)
+    {
+        $this->jwt = $jwt;
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->only('name', 'email', 'password', 'password_confirmation'), [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:4|confirmed'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['success' => 0, 'message' => 'Please fix these errors', 'errors' => $validator->errors()], 500);
+        }
+
+        try {
+
+            $user = new User();
+            $user->name = $request->input('name');
+            $user->email = $request->input('email');
+            $plainPassword = $request->input('password');
+            $user->password = app('hash')->make($plainPassword);
+
+            $user->save();
+
+            $token = auth()->tokenById($user->id);
+
+            return response()->json([
+                'success' => 1,
+                'message' => 'User Registration success!',
+                'access_token' => $token,
+                'user' => $user
+            ]);
+        } catch (\Exception $e) {
+            //return error message
+            return response()->json(['success' => 0, 'message' => 'User Registration Failed!'], 409);
+        }
+    }
+}
