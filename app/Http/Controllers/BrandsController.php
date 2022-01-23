a7b76dbfd5d4bcdadff296bc8dafa75ad3b7dd7d
AxelTahmid <axel.tahmid@gmail.com>
2022-01-23 18:18:12 +0600

    added brands controller

---
+++ app/Http/Controllers/BrandsController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Brand;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class BrandsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('super_admin_check:store-update-destroy');
+    }
+
+    public function index(Request $request)
+    {
+        $brands = $this->filterAndResponse($request);
+
+        return response()->json(['brands' => $brands], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->only('title'), [
+            'title' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['success' => 0, 'message' => 'Please fix these errors', 'errors' => $validator->errors()], 500);
+        }
+
+        $brand = Brand::create($request->all());
+
+        return response()->json(['success' => 1, 'message' => 'Created successfully', 'brand' => $brand], 201);
+    }
+
+    public function show($id)
+    {
+        $brand = Brand::findOrFail($id);
+
+        return response()->json(['brand' => $brand], 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $brand = Brand::findOrFail($id);
+
+        $validator = Validator::make($request->only('title'), [
+            'title' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['success' => 0, 'message' => 'Please fix these errors', 'errors' => $validator->errors()], 500);
+        }
+
+        $brand->title = $request->input('title');
+
+        $brand->save();
+
+        return response()->json(['success' => 1, 'message' => 'Updated successfully', 'brand' => $brand], 200);
+    }
+
+    public function destroy($id)
+    {
+        $brand = Brand::findOrFail($id);
+
+        $brand->delete();
+
+        return response()->json(['success' => 1, 'message' => 'Deleted successfully'], 200);
+    }
+
+    /**
+     * @param Request $request
+     */
+    protected function filterAndResponse(Request $request)
+    {
+        $query = Brand::whereRaw("1=1");
+
+        if ($request->has('all')) {
+            return $query->get();
+        }
+
+        if ($request->id) {
+            $query->where('id', $request->id);
+        }
+
+        if ($request->title) {
+            $query->where('title', 'like', "%" . $request->title . "%");
+        }
+
+        $brands = $query->paginate(10);
+
+        return $brands;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,15 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->post('/login', 'Auth\\LoginController@login');
     $router->post('/register', 'Auth\\RegisterController@register');
 
-
     $router->group(['prefix' => 'category'], function () use ($router) {
         $router->get('/', 'CategoriesController@index');
         $router->get('/htmltree', 'CategoriesController@getCategoryHtmlTree');
         $router->get('/{id}', 'CategoriesController@show');
+    });
+
+    $router->group(['prefix' => 'brand'], function () use ($router) {
+        $router->get('/', 'BrandsController@index');
+        $router->get('/{id}', 'BrandsController@show');
     });
 
     $router->group(['middleware' => 'auth:api'], function () use ($router) {
@@ -37,6 +41,12 @@ $router->group(['prefix' => 'api'], function () use ($router) {
             $router->post('/', 'CategoriesController@store');
             $router->put('/{id}', 'CategoriesController@update');
             $router->delete('/{id}', 'CategoriesController@destroy');
+        });
+
+        $router->group(['prefix' => 'brand'], function () use ($router) {
+            $router->post('/', 'BrandsController@store');
+            $router->put('/{id}', 'BrandsController@update');
+            $router->delete('/{id}', 'BrandsController@destroy');
         });
     });
 });
