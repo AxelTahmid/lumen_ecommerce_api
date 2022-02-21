3caf093bc03949e1eb4a8cc92e43a4682abae724
AxelTahmid <axel.tahmid@gmail.com>
2022-02-21 19:16:31 +0600

    added cron job in commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\ClearCartCommand::class,
     ];
 
     /**
@@ -24,6 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //
+        // run every 15 minutes
+        $schedule->command('clear:cart')->cron('*/15 * * * *');
     }
 }
